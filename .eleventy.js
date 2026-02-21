37a1e7caeb1454fe0d3e41c6aba37b7542b80cb3
Visionsoundhub <studiovisionsound@gmail.com>
2026-02-21 22:19:46 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 module.exports = function(eleventyConfig) {
   
+  // Φίλτρο για να διαβάζει σωστά το Markdown (Εικόνες, Bold, Παραγράφους)
+  eleventyConfig.addFilter("markdown", function(value) {
+      if (!value) return "";
+      let markdown = require("markdown-it")({ html: true, breaks: true });
+      return markdown.render(value);
+  });
+
   // Λέμε στο σύστημα να αντιγράφει φώτο, ήχο και το admin panel όπως είναι
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
